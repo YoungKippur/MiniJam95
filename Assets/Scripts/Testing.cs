677defb221ec2a3558c8da7a2b45c0f64d484529
MateCon <68882595+MateCon@users.noreply.github.com>
2021-12-11 00:40:54 -0300

    create grid

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,7 +4,30 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    Grid grid; 
+
     void Start() {
-        Grid grid = new Grid(20, 10, 2f);
+        grid = new Grid(gameObject, 10, 5, 2f, new Vector3(5, 5, 0));
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            grid.SetValue(GetMouseWorldPosition(), 56);
+        }
+
+        if(Input.GetMouseButtonDown(1)) {
+            Debug.Log(grid.GetValue(GetMouseWorldPosition()));
+        }
+    }
+
+    public Vector3 GetMouseWorldPosition() {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public Vector3 GetMouseWorldPositionWithZ(Vector3 mousePosition, Camera camera) {
+        Vector3 worldPosition = camera.ScreenToWorldPoint(mousePosition);
+        return worldPosition;
     }
 }
