b32c1b74ec08e0fbbcfb99a41849d2ce5c73885d
Kippur <marcos.strauss.ort@gmail.com>
2021-12-12 01:35:48 -0300

    .

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -35,8 +35,4 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
         }
     }
-
-    public void Teleport(Vector3 vector){
-        transform.localPosition = vector;
-    }
 }
--- Assets/Scripts/StartTP.cs
+++ Assets/Scripts/StartTP.cs
@@ -2,19 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartTP : MonoBehaviour
-{
+public class StartTP : MonoBehaviour{
     GameObject Player, EndTeleport;
+    Animator animator;
+    private bool portal = false, portal2 = false;
+    private float timer;
+    public float interval = 2f;
 
     void Start(){
         EndTeleport = GameObject.Find("End");
         Player = GameObject.Find("Player");
+        animator = Player.GetComponent<Animator>();
+        timer = interval;
+    }
+
+    void Update(){
+        if (portal){
+            timer -= Time.deltaTime;
+            if(timer <= 0){
+                Debug.Log("TP");
+                Player.transform.localPosition = EndTeleport.transform.localPosition;
+                timer = interval;
+                animator.SetBool("isPortal", false);
+                animator.SetBool("isPortal2", true);
+                portal2 = true;
+            }
+        }
+        else if (portal2){
+            timer -= Time.deltaTime;
+            if(timer <= 0){
+                timer = interval;
+                animator.SetBool("isPortal2", false);
+                portal2 = false;
+            }
+        }
+        else timer = interval;
     }
 
     void OnTriggerEnter2D(Collider2D collider){
         if (collider.gameObject.tag == "Player"){
-            Debug.Log("TP");
-            Player.transform.localPosition = EndTeleport.transform.localPosition;
+            portal = true;
+            animator.SetBool("isPortal", true);
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider){
+        if (collider.gameObject.tag == "Player"){
+            portal = false;
+            animator.SetBool("isPortal", false);
+            timer = interval;
         }
     }
 }
